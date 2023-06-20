0206233c0bbcde5204283c85f606b3095dbcde4d
21007509 <21007509@myrp.edu.sg>
2023-06-20 16:24:36 +0800

    Template

---
+++ IoTWebApplication/Models/SensorsData.cs
@@ -0,0 +1,10 @@
+﻿namespace IoTWebApplication.Models
+{
+    public class SensorsData
+    {
+        public decimal Temperature { get; set; }
+        public decimal Humidity { get; set; }
+        public decimal CO2 { get; set; }
+    }
+}
+
